f8b089f5f9283d1783b8b9e220d38ab9ab8553c9
Pulkit0707 <pulkit0707chawla@gmail.com>
2025-07-17 00:51:46 +0530

    write code for models and controllers

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,17 +1,121 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"restraunt-go/database"
+	"restraunt-go/models"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
+var validate = validator.New()
 
 func GetFoods() gin.HandlerFunc{
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		recordPerPage,err:=strconv.Atoi(c.Query("recordPerPage"))
+		if err!=nil || recordPerPage<1{
+			recordPerPage=10
+		}
+		page,err:=strconv.Atoi(c.Query("page"))
+		if err!=nil || page<1{
+			page=1
+		}
+		startIdx := (page-1)*recordPerPage
+		startIdx, err = strconv.Atoi(c.Query("startIdx"))
+		matchStage := bson.D{{"$match",bson.D{{}}}}
+		groupStage := bson.D{
+			{"$group", bson.D{
+				{"_id", nil},
+				{"total_count", bson.D{{"$sum", 1}}},
+			}},
+		}
+		projectStage := bson.D{
+			{
+				"$project", bson.D{
+					{"_id",0},
+					{"total_count",1},
+					{"food_items", bson.D{{"$slice", []interface{}{"$state", startIdx, recordPerPage}}}}
+				}
+			}
+		}
+		result,err:=foodCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage, groupStage, projectStage
+		})
+		defer cancel()
+		if err!=nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"error occured while listing food items"})
+		}
+		var allFoods []bson.M
+		if err = result.All(ctx,&allFoods); err!=nil{
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOk,allFoods[0])
+	}
 }
 
 func GetFood() gin.HandlerFunc{
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx,cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		foodId:=c.Param("food_id")
+		var food models.Food
+		err:=foodCollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
+		defer cancel()
+		if err!=nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the food item"})
+		}
+		c.JSON(http.StatusOK,food)
+	}
 }
 
 func CreateFood() gin.HandlerFunc{
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx,cancel=context.WithTimeout(context.Background(), 100*time.Second)
+		var menu models.Menu
+		var food models.Food
+		if err := c.BindJSON(&food); err!=nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		validationErr:=validate.Struct(food)
+		if validationErr!=nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+		err:=menuCollection.FindOne(ctx,bson.M{"menu_id": food.Menu_id}).Decode(&menu)
+		defer cancel()
+		if err!=nil{
+			msg:=fmt.Sprintf("menu was not found")
+			c.JSON(http.StatusInternalServerError, gin.H{"error":msg})
+			return
+		}
+		food.Created_at,_ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		food.Updated_at,_ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		food.ID = primitive.NewObjectID()
+		food_id := food.ID.Hex()
+		food.Food_id = &food_id
+		var num = toFixed(*food.Price, 2)
+		food.Price = &num
+		result, insertErr := foodCollection.InsertOne(ctx, food)
+		if insertErr!= nil{
+			msg := fmt.Sprintf("Food item was not created")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return;
+		}
+		defer cancel()
+		c.JSON(http.StatusOK, result)
+	}
 }
 
 func round(num float64) int {}
@@ -19,5 +123,8 @@ func round(num float64) int {}
 func toFixed(num float64, precision int) float64{}
 
 func UpdateFood() gin.HandlerFunc{
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(),100*time.Second)
+		var food models.Food
+	}
 }
